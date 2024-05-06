e452aea88ec9401bd54ff4f747f6a8d0b324e1c8
Sam Paniccia <paniccia.s@northeastern.edu>
2024-05-06 00:57:51 -0400

    UNSAFE Demo "console":
    - Add entries and see them in a list
    - Rocket state needs to replace the unsafe stuff

--- panicvtt_frontend/src/main.rs
+++ panicvtt_frontend/src/main.rs
@@ -1,8 +1,11 @@
 #[macro_use] extern crate rocket; 
 use panicvtt_engine::{self, entities::entity::Entity};
 
-#[get("/")]
-fn index() -> String {
+use rocket::{form::Form, response::Redirect};
+use rocket_dyn_templates::{Template, context};
+
+//#[get("/")]
+fn old_index() -> String {
     let mut data = String::new();
     
     data.push_str(&format!("PanicVTT version {}\n", panicvtt_engine::version()));
@@ -13,7 +16,39 @@ fn index() -> String {
     data
 }
 
+#[derive(FromForm)]
+struct Command<'r> {
+    command: &'r str
+}
+
+static mut COMMANDS: Vec<String> = Vec::new(); 
+
+#[get("/")]
+fn index() -> Template { 
+    Template::render("index", context! { 
+        items: unsafe { COMMANDS.clone() }
+     })
+}
+
+#[post("/", data = "<form_data>")]
+fn add_command(form_data: Option<Form<Command<'_>>>) -> Redirect {
+    match form_data {
+        Some(f) => {
+            println!("Got {}", f.command); 
+            unsafe {
+                COMMANDS.push(String::from(f.command));
+            }
+        },
+        None => {
+            println!("??");
+        }
+    }
+    Redirect::to("/")
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+    .mount("/", routes![index, add_command])
+    .attach(Template::fairing())
 }
